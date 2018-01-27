d98d9c68e878cc0717155e07690b7f5b590a1cd7
Rahul Bethi <rahulbethi@hotmail.com>
2018-01-27 00:04:33 -0800

    Added Oculus, Removed Steam
    Added OVRGrabbable for grabbable objects, Added OVRGrabber for hands.

---
+++ Project/Assets/_PerformanceBounceback/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public bool showDebug;
+
+	/*//   S T A R T																										
+	void Start()
+	{
+		
+	}*/
+	
+	//   U P D A T E																									
+	void Update()
+	{
+		if(Time.frameCount % 30 == 0)
+		{
+			System.GC.Collect();
+		}
+	}
+
+	public void Debug_Log(string log)
+	{
+		if(showDebug)
+			Debug.Log(log);
+	}
+}
